a3fe2aa2cb3702552db48d2e06273af7e7b57fb3
Clare Aine Davies <clare.aine.davies@intel.com>
2022-09-01 11:21:23 +0300

    able to run a unit test for user

---
+++ src/user/user.service.unit-spec.ts
@@ -0,0 +1,43 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UserService } from '../user/user.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { EditUserDto } from '../user/dto';
+
+describe('UserService Unit Test', () => {
+  let prisma: PrismaService;
+  let service: UserService;
+
+  beforeEach(async () => {
+    const dto: EditUserDto = {
+      email: 'vlad@gmail.com',
+      firstName: 'djndj',
+      lastName: 'djsjd'
+    };
+    
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [UserService, 
+        {
+        provide: PrismaService,
+        // notice that only the functions we call from the model are mocked
+        useValue: {
+          new: jest.fn().mockResolvedValue(dto),
+          constructor: jest.fn().mockResolvedValue(dto),
+          find: jest.fn(),
+          findOne: jest.fn(),
+          update: jest.fn(),
+          create: jest.fn(),
+          remove: jest.fn(),
+          exec: jest.fn(),
+        },
+      },],
+    }).compile();
+
+
+    service = module.get<UserService>(UserService);
+    prisma = module.get<PrismaService>(PrismaService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
